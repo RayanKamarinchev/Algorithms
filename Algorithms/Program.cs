9a438bdb27fd197bf6131dcce00e43880986dae5
RayanKamarinchev <rayan2008bg@gmail.com>
2022-07-10 22:06:15 +0300

    Searching algorithms

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> numbers = new List<int>();
+int max = 10;
+for (int i = 1; i <= max; i++)
+{
+    numbers.Add(i);
+}
+
+Console.WriteLine(LinearSearch(numbers, 7));
+Console.WriteLine(BinarySearch(numbers, 0));
+
+static double LinearSearch(List<int> nums, int value)
+{
+    for (int i = 0; i < nums.Count; i++)
+    {
+        if (nums[i] == value)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+static double BinarySearch(List<int> nums, int value)
+{
+    int left = 0;
+    int right = nums.Count - 1;
+    while (left <= right)
+    {
+        int index = (left + right) / 2;
+        if (nums[index] > value)
+        {
+            right = index - 1;
+        }
+        else if (nums[index] < value)
+        {
+            left = index + 1;
+        }
+        else
+        {
+            return index;
+        }
+    }
+
+    return -1;
+}
